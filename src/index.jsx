3c917a1d3b1c5012a758d784cbae2aa3017d3572
Neil Sutcliffe <neil@neilsutcliffe.com>
2019-03-20 19:27:16 +0100

    Fixed issue and changed default name

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 import defaultStore from './stores';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
